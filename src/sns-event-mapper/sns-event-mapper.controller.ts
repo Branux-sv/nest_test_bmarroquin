026a730e9ab95d8977f4ea19a69310bc2e42cf9d
Brayan Marroquin <eduar.gol07@gmail.com>
2025-10-19 17:39:14 -0600

    feat: add minor change to return http 200 instead of 201

--- src/sns-event-mapper/sns-event-mapper.controller.ts
+++ src/sns-event-mapper/sns-event-mapper.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { SNSEventMapperService } from './sns-event-mapper.service';
 import { SNSEventMapperResponseDto } from './dto/sns-event-mapper-response.dto';
 import { SesSnsEvent } from './sns-event-mapper.entity';
@@ -8,6 +8,7 @@ export class SNSEventMapperController {
   constructor(private readonly snsEventMapperService: SNSEventMapperService) {}
 
   @Post()
+  @HttpCode(HttpStatus.OK)
   getSnsEventMappedResponse(
     @Body() sesSnsEvent: SesSnsEvent,
   ): SNSEventMapperResponseDto[] {
