2992d7267dd77e81376d36b3915de387556f6b13
Brayan Marroquin <eduar.gol07@gmail.com>
2025-10-19 00:22:56 -0600

    feat: add mail parser module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SNSEventMapperModule } from './sns-event-mapper/sns-event-mapper.module';
+import { MailParserModule } from './mail-parser/mail-parser.module';
 @Module({
-  imports: [SNSEventMapperModule],
+  imports: [SNSEventMapperModule, MailParserModule],
 })
 export class AppModule {}
